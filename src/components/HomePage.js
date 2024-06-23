55c58b0bac75048f86d3d6c3a3c301958040de98
Ronnfaed <prinzantiochus@gmail.com>
2024-06-23 04:31:07 -0300

    HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,18 @@
+// HomePage.jsx
+import heart from '../assets/heart.png'
 import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
     return (
-        <div>
-            <h2>HOOME</h2>
-            <p>Conteúdo</p>
+        <div className="homepage-container">
+            <div className="welcome-text">
+                <h1>Bem-Vindo!</h1>
+                <p>Navegue entre as páginas e funcionalidades através da barra de navegação lateral.</p>
+            </div>
+            <div className="image-container">
+                <img src={heart} alt="Imagem de boas-vindas" />
+            </div>
         </div>
     );
 };
